295582c07e73c9ee3e5cfaffe2f446f5039b76c8
Daniel Neuhäuser <ich@danielneuhaeuser.de>
2016-07-14 22:42:28 +0200

    add basic module

---
+++ hybrid_attributes.py
@@ -0,0 +1,11 @@
+"""
+    hybrid_attributes
+    ~~~~~~~~~~~~~~~~~
+
+    :copyright: 2016 by Daniel Neuhäuser
+    :license: BSD, see LICENSE.rst
+"""
+
+
+__version__ = '0.1.0'
+__version_info__ = (0, 1, 0)
--- setup.py
+++ setup.py
@@ -15,17 +15,29 @@ from setuptools import setup
 PROJECT_PATH = Path(__file__).parent.absolute()
 
 
-setup(
-    name='hybrid-attributes',
-    version='0.1.0',
-    description='Hybrid attributes',
-    long_description=(PROJECT_PATH / 'README.rst').read_text('utf-8'),
-    url='https://github.com/DasIch/hybrid-attributes/',
-    author='Daniel Neuhäuser',
-    author_email='ich@danielneuhaeuser.de',
-    classifiers=[
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython'
-    ]
-)
+_version_re = re.compile("__version__ = '(\d+\.\d+\.\d+)'")
+def get_version():
+    with (PROJECT_PATH / 'hybrid_attributes.py').open(encoding='utf-8') as f:
+        for line in f:
+            match = _version_re.match(line)
+            if match:
+                return match.group(1)
+    raise ValueError('__version__ not found')
+
+
+if __name__ == '__main__':
+    setup(
+        name='hybrid-attributes',
+        version=get_version(),
+        description='Hybrid attributes',
+        long_description=(PROJECT_PATH / 'README.rst').read_text('utf-8'),
+        url='https://github.com/DasIch/hybrid-attributes/',
+        author='Daniel Neuhäuser',
+        author_email='ich@danielneuhaeuser.de',
+        classifiers=[
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: Implementation :: CPython'
+        ],
+        py_modules=['hybrid_attributes']
+    )
