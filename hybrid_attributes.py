a6318b72b9878162e3e6fb44b3db392156ed9e34
Daniel Neuhäuser <ich@danielneuhaeuser.de>
2016-07-15 01:12:19 +0200

    add hybrid_property

--- hybrid_attributes.py
+++ hybrid_attributes.py
@@ -9,3 +9,113 @@
 
 __version__ = '0.1.0'
 __version_info__ = (0, 1, 0)
+
+
+class hybrid_property:
+    """
+    A `property` like descriptor that gives you the ability to produce values
+    when accessed on a class.
+
+    :param fget:
+        A function that is called with the instance or class the property is
+        accessed on, the result will be returned as attribute.
+
+    :param fset:
+        A function that will be called when a value is assigned to the
+        attribute on an instance.
+
+    :param fdel:
+        A function that is called when the attribute is deleted on an instance.
+
+    :param fcget:
+        A function that is called when the attribute is accessed on a class,
+        with that class as argument.
+
+        Use this when you need to do something different, depending on whether
+        the attribute is accessed on a class or an instance.
+
+    If any of these functions are missing, an :exc:`AttributeError` will be
+    raised when a user performs an operation that would otherwise lead to such
+    a function being called.
+
+    Instead of passing all arguments directly, you should use decorators to
+    define the different functions::
+
+        class SomeClass:
+            @hybrid_property
+            def attribute(self):
+                return self._attribute
+
+            @attribute.classgetter
+            def attribute(cls):
+                return 'SomeClass.attribute was accessed'
+
+            @attribute.setter
+            def attribute(self, value):
+                self._attribute = value
+
+            @attribute.deleter
+            def attribute(self, value):
+                del self._attribute
+    """
+    def __init__(self, fget=None, fset=None, fdel=None, fcget=None):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
+        self.fcget = fcget
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            if self.fcget is None and self.fget is None:
+                raise AttributeError('unreadable attribute')
+            return (self.fcget or self.fget)(owner)
+        else:
+            if self.fget is None:
+                raise AttributeError('unreadable attribute')
+            return self.fget(instance)
+
+    def __set__(self, instance, value):
+        if self.fset is None:
+            raise AttributeError("can't set attribute")
+        self.fset(instance, value)
+
+    def __delete__(self, instance):
+        if self.fdel is None:
+            raise AttributeError("can't delete attribute")
+        self.fdel(instance)
+
+    def getter(self, fget):
+        """
+        Returns a new `hybrid_property` instance with `fget` set to the given
+        function.
+        """
+        return self._copy(fget=fget)
+
+    def setter(self, fset):
+        """
+        Returns a new `hybrid_property` instance with `fset` set to the given
+        function.
+        """
+        return self._copy(fset=fset)
+
+    def deleter(self, fdel):
+        """
+        Returns a new `hybrid_property` instance with `fdel` set to the given
+        function.
+        """
+        return self._copy(fdel=fdel)
+
+    def classgetter(self, fcget):
+        """
+        Returns a new `hybrid_property` instance with `fcget` set to the given
+        function.
+        """
+        return self._copy(fcget=fcget)
+
+    def _copy(self, fget=None, fset=None, fdel=None, fcget=None):
+        return self.__class__(
+            fget=self.fget if fget is None else fget,
+            fset=self.fset if fset is None else fset,
+            fdel=self.fdel if fdel is None else fdel,
+            fcget=self.fcget if fcget is None else fcget
+        )
--- test_hybrid_attributes.py
+++ test_hybrid_attributes.py
@@ -5,3 +5,165 @@
     :copyright: 2016 by Daniel Neuhäuser
     :license: BSD, see LICENSE.rst for details
 """
+import pytest
+
+from hybrid_attributes import hybrid_property
+
+
+class TestHybridProperty:
+    """
+    Tests for the `hybrid_property` descriptor.
+    """
+
+    def test_get_on_instance(self):
+        class Foo:
+            @hybrid_property
+            def spam(self):
+                assert isinstance(self, Foo)
+                return 'spam'
+
+        assert Foo().spam == 'spam'
+
+    def test_get_on_class(self):
+        class Foo:
+            @hybrid_property
+            def spam(self):
+                assert self is Foo
+                return 'spam'
+
+        assert Foo.spam == 'spam'
+
+    def test_get_without_fget(self):
+        class Foo:
+            spam = hybrid_property()
+
+        with pytest.raises(AttributeError) as exc_info:
+            Foo().spam
+
+        assert len(exc_info.value.args) == 1
+        assert exc_info.value.args[0] == 'unreadable attribute'
+
+    def test_get_without_fget_on_class(self):
+        class Foo:
+            spam = hybrid_property()
+
+        with pytest.raises(AttributeError) as exc_info:
+            Foo.spam
+
+        assert len(exc_info.value.args) == 1
+        assert exc_info.value.args[0] == 'unreadable attribute'
+
+    def test_set_on_instance(self):
+        class Foo:
+            def _set_spam(self, value):
+                assert isinstance(self, Foo)
+                self._spam = value
+
+            spam = hybrid_property(fset=_set_spam)
+
+        foo = Foo()
+        foo.spam = 1
+        assert foo._spam == 1
+
+    def test_set_without_fset(self):
+        class Foo:
+            spam = hybrid_property()
+
+        foo = Foo()
+        with pytest.raises(AttributeError) as exc_info:
+            foo.spam = 1
+
+        assert len(exc_info.value.args) == 1
+        assert exc_info.value.args[0] == "can't set attribute"
+
+    def test_delete_on_instance(self):
+        class Foo:
+            def _delete_spam(self):
+                assert isinstance(self, Foo)
+                self._delete_was_called = True
+
+            spam = hybrid_property(fdel=_delete_spam)
+
+        foo = Foo()
+        del foo.spam
+        assert foo._delete_was_called
+
+    def test_delete_without_fdel(self):
+        class Foo:
+            spam = hybrid_property()
+
+        foo = Foo()
+        with pytest.raises(AttributeError) as exc_info:
+            del foo.spam
+
+        assert len(exc_info.value.args) == 1
+        assert exc_info.value.args[0] == "can't delete attribute"
+
+    def test_get_with_fcget(self):
+        class Foo:
+            def _get_spam(self):
+                assert self is Foo
+                return 'spam'
+
+            spam = hybrid_property(fcget=_get_spam)
+
+        assert Foo.spam == 'spam'
+
+    def test_get_with_fcget_has_priority(self):
+        class Foo:
+            def _get_spam(self):
+                assert isinstance(self, Foo)
+                return 1
+
+            def _cget_spam(self):
+                assert self is Foo
+                return 2
+
+            spam = hybrid_property(fget=_get_spam, fcget=_cget_spam)
+
+        assert Foo().spam == 1
+        assert Foo.spam == 2
+
+    def test_getter(self):
+        class Foo:
+            p = hybrid_property()
+
+            @p.getter
+            def spam(self):
+                return 'spam'
+
+        assert Foo.spam == 'spam'
+
+    def test_setter(self):
+        class Foo:
+            p = hybrid_property()
+
+            @p.setter
+            def spam(self, value):
+                self._spam = value
+
+        foo = Foo()
+        foo.spam = 1
+        assert foo._spam == 1
+
+    def test_deleter(self):
+        class Foo:
+            p = hybrid_property()
+
+            @p.deleter
+            def spam(self):
+                self._spam_is_deleted = True
+
+        foo = Foo()
+        del foo.spam
+        assert foo._spam_is_deleted
+
+    def test_classgetter(self):
+        class Foo:
+            p = hybrid_property()
+
+            @p.classgetter
+            def spam(self):
+                return 'spam'
+
+        assert Foo.spam == 'spam'
